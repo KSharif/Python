777c4263cddf34f1e69ca6867d1c52d7eb7f4ff0
Kazi Sharif <89753698+KSharif@users.noreply.github.com>
2024-03-21 16:13:30 -0500

    Add files via upload

---
+++ pythonlearning.py
@@ -0,0 +1,26 @@
+# start learning with Hello world 
+print ("Hello world")
+
+# taking input 
+# Ask user for their Name 
+name = input("what is your name \n ")
+print (name)
+print ("Hello,")
+
+# multiple comments technique """-----""""
+
+#another way to add '+'
+
+print ("------------------------")
+print("hello, " + name )
+print ("------------------------")
+print("hello," , name )
+
+#Remove whitespace from str and capilatization user's name
+
+name = name.strip().title()
+print (f" hello , {name}")
+
+
+
+"""  Parameters  """
